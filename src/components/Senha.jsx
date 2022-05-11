a8f6b3a828a2b3cca798d8eb593c0f0d3c8967d7
Jean Victor <jeanvictor.lopes@gmail.com>
2022-05-11 08:04:47 -0300

    senha funcional

--- src/components/Senha.jsx
+++ src/components/Senha.jsx
@@ -1,6 +1,35 @@
+import React from "react";
+
 export function Senha() {
-    
-    return(
-        <h1 className="senha">0</h1>
+    const [senha, setSenha] = React.useState(0);
+
+    function add() {
+        setSenha(senha + 1)
+    }
+
+    function back() {
+        setSenha(senha - 1)
+    }
+
+    const handleKeyDown = (event) => {
+        const keyName = event.key;
+        const keyC = event.keyCode;
+
+        if (keyName === "0") {
+            add();
+        }
+        if (keyName === "5") {
+            back();
+        }
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown)
+
+        return () => { window.removeEventListener('keydown', handleKeyDown) }
+    })
+
+    return (
+        <h1 className="senha">{senha}</h1>
     )
 }
